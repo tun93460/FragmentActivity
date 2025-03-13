da94ef80db65ab6f9c4170be03ea2b3f3b0cb48d
tun93460 <ldgraham2002@gmail.com>
2025-03-13 11:30:09 -0400

    Moved logic to on view created

--- app/src/main/java/edu/temple/fragmentactivity/ColorFragment.kt
+++ app/src/main/java/edu/temple/fragmentactivity/ColorFragment.kt
@@ -1,27 +1,51 @@
 package edu.temple.fragmentactivity
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 
 
 class ColorFragment : Fragment() {
 
-
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_color, container, false).apply {
+        return inflater.inflate(R.layout.fragment_color, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.apply {
             findViewById<Spinner>(R.id.spinner).apply {
                 adapter = ArrayAdapter(context, android.R.layout.simple_spinner_item,
                     arrayOf("Blue", "Red", "White", "Yellow", "Magenta", "Yellow"))
+
+                onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                    override fun onItemSelected(
+                        parent: AdapterView<*>?,
+                        view: View?,
+                        position: Int,
+                        id: Long
+                    ) {
+                        parent?.run {
+                            view?.setBackgroundColor(Color.parseColor(getItemAtPosition(position).toString()))
+                        }
+                    }
+
+                    override fun onNothingSelected(parent: AdapterView<*>?) {
+
+                    }
+
+                }
             }
         }
     }
